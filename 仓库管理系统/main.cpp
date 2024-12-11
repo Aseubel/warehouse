28be80a63afee99404a88f12b0378357f8a4aed4
Aseubel <1228392319@qq.com>
2024-12-11 16:02:25 +0800

    添加商品索引和代码的映射表，添加最低阈值相关功能

--- 仓库管理系统/main.cpp
+++ 仓库管理系统/main.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <algorithm>
 #include <iomanip>
+#include <unordered_map>
 using namespace std;
 
 // 商品结构体
@@ -16,7 +17,8 @@ struct Goods {
     double price;       // 商品价格
     int totalQuantity;  // 商品总量
     time_t expirationDate;  // 保质期时间（以时间戳表示）
-    vector<pair<time_t, int>> inOutRecords;  // 入库时间、入库数量记录，也可用于出库记录
+    vector<pair<time_t, int>> inOutRecords;  // 入库时间、入库数量记录，也可用于出库记录（数量为负数表示出库）
+    int minStock;  // 最低库存量
 };
 
 // 出入库结构体，用于记录每一次出入库操作详情
@@ -59,6 +61,9 @@ public:
 };
 
 class WarehouseManagement {
+private:
+    unordered_map<string, int> codeIndexMap;  // 商品代码与索引的映射表
+
 public:
     vector<Goods> goodsList;  // 存储商品信息的列表
 
@@ -80,7 +85,6 @@ public:
                 ss.ignore();
                 ss >> good.totalQuantity;
                 ss.ignore();
-                // TODO: 解析时间等其他信息
                 ss >> good.expirationDate;
                 ss.ignore();
                 int recordCount;
@@ -97,6 +101,8 @@ public:
                     inOutRecords.push_back(make_pair(inOutTimeStr, quantity));
                 }
                 good.inOutRecords = inOutRecords;
+                ss >> good.minStock;
+                ss.ignore();
                 goodsList.push_back(good);
             }
             file.close();
@@ -115,7 +121,7 @@ public:
                 for (const auto& record : good.inOutRecords) {
                     file << record.first << "," << record.second << ",";
                 }
-                file << endl;
+                file << good.minStock << endl;
             }
             file.close();
         }
@@ -123,16 +129,23 @@ public:
 
     // 根据商品代码查找商品在列表中的索引
     int findGoodsIndex(const string& code) {
-        for (size_t i = 0; i < goodsList.size(); ++i) {
-            if (goodsList[i].code == code) {
-                return i;
-            }
+        if (codeIndexMap.find(code) != codeIndexMap.end()) {
+            return codeIndexMap[code];
         }
         return -1;
     }
 
+    // 建立商品代码与索引的映射表
+    void buildCodeIndexMap() {
+        for (size_t i = 0; i < goodsList.size(); ++i) {
+            codeIndexMap[goodsList[i].code] = i;
+        }
+    }
+
+    // 构造函数，从文件中读取商品信息初始化仓库
     WarehouseManagement(const string& fileName) {
         loadGoodsFromFile(fileName);
+        buildCodeIndexMap();
     }
 
     // 商品入库功能
@@ -141,6 +154,9 @@ public:
         if (index != -1) {
             goodsList[index].totalQuantity += record.quantity;
             goodsList[index].inOutRecords.push_back(make_pair(record.inOutTime, record.quantity));
+            if (goodsList[index].totalQuantity < goodsList[index].minStock) {
+                cout << "警告：商品库存仍然低于最低库存阈值！" << endl;
+            }
         }
         else {
             Goods newGood = { record.code, "", "", 0, record.quantity, 0, {make_pair(record.inOutTime, record.quantity)} };
@@ -151,6 +167,8 @@ public:
             cin >> newGood.category;
             cout << "请输入商品价格: ";
             cin >> newGood.price;
+            cout << "请输入商品最低库存阈值：";
+            cin >> newGood.minStock;
             cout << "请输入保质期（格式：xxxx-xx-xx xx:xx:xx）: ";
             string timeStr;
             getline(cin, timeStr);
@@ -229,6 +247,7 @@ public:
                 cout << "商品类别: " << good.category << endl;
                 cout << "商品价格: " << good.price << endl;
                 cout << "商品总量: " << good.totalQuantity << endl;
+                cout << "最低库存阈值：" << good.minStock << endl;
                 cout << "保质期：" << Util::formatTime(good.expirationDate) << endl;
                 cout << "出入库记录：" << endl;
                 for (const auto& record : good.inOutRecords) {
@@ -308,6 +327,30 @@ public:
             return a.expirationDate < b.expirationDate;
             });
     }
+
+    // 查询库存量低于阈值的商品
+    void displayLowStockGoods() {
+        for (const auto& good : goodsList) {
+            if (good.totalQuantity < good.minStock) {
+                cout << "商品代码: " << good.code << endl;
+                cout << "商品名称: " << good.name << endl;
+                cout << "商品类别: " << good.category << endl;
+                cout << "商品价格: " << good.price << endl;
+                cout << "商品总量: " << good.totalQuantity << endl;
+                cout << "保质期：" << Util::formatTime(good.expirationDate) << endl;
+                cout << "最低库存阈值：" << good.minStock << endl;
+                cout << "-------------------------" << endl;
+            }
+        }
+    }
+
+    // 设置最低库存阈值的函数
+    void setMinStock(const string& code, int minStock) {
+        int index = findGoodsIndex(code);
+        if (index != -1) {
+            goodsList[index].minStock = minStock;
+        }
+    }
 };
 
 int main() {
@@ -327,7 +370,9 @@ int main() {
         cout << "9. 查询出库信息（按日期）" << endl;
         cout << "10. 显示快到保质期商品" << endl;
         cout << "11. 排序功能" << endl;
-        cout << "12. 退出" << endl;
+        cout << "12. 查询所有库存量低于阈值的商品" << endl;
+        cout << "13. 设置商品最低库存阈值" << endl;
+        cout << "14. 退出" << endl;
         cout << "请输入你的选择: ";
         cin >> choice;
 
@@ -446,10 +491,25 @@ int main() {
             }
             break;
         }
-        case 12:
+        case 12: {
+            string code;
+            cout << "请输入商品代码: ";
+            cin >> code;
+            int minStock;
+            cout << "请输入最低库存阈值: ";
+            cin >> minStock;
+            warehouse.setMinStock(code, minStock);
+            break;
+        }
+        case 13: {
+            warehouse.setMinStock(code, minStock);
+            break;
+        }
+        case 14: {
             warehouse.saveGoodsToFile("goods.txt");  // 退出前保存数据到文件
             cout << "感谢使用！" << endl;
             break;
+        }
         default:
             cout << "无效的选择，请重新输入！" << endl;
         }
